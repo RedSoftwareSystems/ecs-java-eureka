b266ae7e67080ca4971872f21df90dfd554b5128
Luca Botti <luca.botti@gmail.com>
2017-08-16 15:48:19 +0200

    Modified Condition, now "ecs" profile

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,14 @@ set -o xtrace
 
 #export EC2_HOST=$(wget -O - http://169.254.169.254/latest/meta-data/local-ipv4 2> /dev/null)
 
-if test $ECS_DEPLOYMENT=true; then
-  echo "var Set, executing python script"
-  result="$(python3 /app/ecs-get-port-mapping.py)"
-  eval "$result"
-  ECS_HOST_PORT="$PORT_TCP_8080"
-  export ECS_HOST_PORT;
-else
-  echo "var is unset"
-fi
-
-
+case "$SPRING_PROFILES_ACTIVE" in
+  *ecs*)
+    echo "var Set, executing python script"
+    result="$(python3 /app/ecs-get-port-mapping.py)"
+    eval "$result"
+    ECS_HOST_PORT="$PORT_TCP_8080"
+    export ECS_HOST_PORT;;
+esac
 
 
 exec "$@"
